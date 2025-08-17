f57b69635f4fb3ab491dbfe02668c7f98d0349cd
v-t-9 <violeta.a.t.9@gmail.com>
2025-08-17 16:02:19 -0300

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   return (
     <>
     <section className="cars">
-      <Card image={import.meta.env.BASE_URL + "src/assets/images/icon-sedan.svg"} title="Sedans" description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+      <Card image={import.meta.env.BASE_URL + "assets/images/icon-sedan.svg"} title="Sedans" description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
   or on your next road trip."/>
-      <Card image={import.meta.env.BASE_URL + "src/assets/images/icon-suvs.svg"} title="SUVs" description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+      <Card image={import.meta.env.BASE_URL + "assets/images/icon-suvs.svg"} title="SUVs" description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
   and off-road adventures."  />
-      <Card image={import.meta.env.BASE_URL + "src/assets/images/icon-luxury.svg"} title="Luxury" description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+      <Card image={import.meta.env.BASE_URL + "assets/images/icon-luxury.svg"} title="Luxury" description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
   rental and arrive in style." />
     </section>
     <Footer />
