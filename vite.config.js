2d0c41fb2faa6ac2bc5fd402af9a3900044ce5a3
v-t-9 <violeta.a.t.9@gmail.com>
2025-08-17 15:34:37 -0300

    3rd commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  base:"https://leta09.github.io/3-column-preview-card-component-main",
 })
