5ce2a59364eadf1ccd0e92dec0fdd348e8bd9a6e
v-t-9 <violeta.a.t.9@gmail.com>
2025-08-17 15:57:40 -0300

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import Card from './Card.jsx'
 import Footer from './Footer.jsx'
 function App() {
   const [count, setCount] = useState(0)
-
+    //const BASE_URL = import.meta.env.BASE_URL;
   return (
     <>
     <section className="cars">
-      <Card image=".\src\assets\images\icon-sedans.svg" title="Sedans" description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+      <Card image={import.meta.env.BASE_URL + "src/assets/images/icon-sedan.svg"} title="Sedans" description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
   or on your next road trip."/>
-      <Card image="./src/assets/images/icon-suvs.svg" title="SUVs" description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+      <Card image={import.meta.env.BASE_URL + "src/assets/images/icon-suvs.svg"} title="SUVs" description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
   and off-road adventures."  />
-      <Card image="./src/assets/images/icon-luxury.svg" title="Luxury" description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+      <Card image={import.meta.env.BASE_URL + "src/assets/images/icon-luxury.svg"} title="Luxury" description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
   rental and arrive in style." />
     </section>
     <Footer />
